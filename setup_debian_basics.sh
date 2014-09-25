ed9967e40002f553baa136d652535065355f3a78
debauer <me@debauer.net>
2014-09-25 12:12:39 +0200

    edit setup_domain_basics

--- setup_debian_basics.sh
+++ setup_debian_basics.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # erstma updaten
+echo "sourcen updaten"
 aptitude -y update 1>/dev/null
+echo "sourcen geupdatet"
+echo ""
 # add more cool tools
+echo "installiere htop iftop mc python3"
 aptitude install htop iftop mc python3
-
+echo "done"
+echo ""
 #echo "deb http://http.debian.org/debian jessie main non-free contrib" >> /etc/apt/sources.list
 #echo "deb http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list
 
@@ -14,6 +19,17 @@ echo "#" >> /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
 echo "deb http://http.debian.org/debian sid main non-free contrib" >> /etc/apt/sources.list
 
+echo "sid in sources.list hinzugefügt"
+echo "NEW LINE: deb http://http.debian.org/debian sid main non-free contrib"
+
+echo ""
+echo "wheezy als default repository gesetzt"
+echo "FILE: /etc/apt/apt.conf.d/90binwheezy"
+echo "NEW LINE: APT::Default-Release \"wheezy\";"
 echo "APT::Default-Release \"wheezy\";" >> /etc/apt/apt.conf.d/90binwheezy
 
+echo ""
+echo "MOTD geleert"
 echo "" > /etc/motd
+echo ""
+echo "setup_domain_basics fertig"
