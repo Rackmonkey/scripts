43ef6e0df8de748d896c841ac6dda49a6ff4172c
David Bauer <me@debauer.net>
2015-03-03 13:11:10 +0100

    meteor helper extended

--- meteor.py
+++ meteor.py
@@ -3,21 +3,25 @@
 #
 # Helper for meteor environment
 #
-# STATUS: jooow
+# STATUS: untested
 ##
 
 version = 0.1
 
+import socket
 import helper.basic as basics
 import sys, os
 import argparse
 import configparser
 import subprocess
 
+config = configparser.ConfigParser()
+
 parser = argparse.ArgumentParser(description='node.js application helper')
 parser.add_argument("-b", "--build", help="build meteor app before running", default=False, action="store_true")
-parser.add_argument("-m", "--pm2", help="run with pm2", default=False, action="store_true")
+parser.add_argument("--pm2", help="run with pm2", default=False, action="store_true")
 parser.add_argument("-r", "--run", help="run app", default=False, action="store_true")
+parser.add_argument("-i", "--init", help="run app", default=False, action="store_true")
 parser.add_argument("-p", "--port", type=int, help="port for node app", required=True)
 parser.add_argument("-e", "--env", help="develop OR production", default="develop")
 
@@ -29,6 +33,11 @@ def check_port(port):
 	sock.close()
 	return result
 
+def check_file(path,file):
+	if not (os.path.isfile(path + file)):
+		return 0
+	else:
+		return 1
 
 args = vars(parser.parse_args())
 path_script = os.getcwd()
@@ -38,26 +47,56 @@ folder_dev = "develop"
 path_dev = path_env + "/" + folder_dev
 folder_prod = "app"
 path_prod = path_env + "/" + folder_prod
+folder_config = "config"
+path_config = path_env + "/" + folder_config
 
-basics.make_ordner(path_prod)
-basics.make_ordner(path_dev)
+if(args["init"]):
+	basics.make_ordner(path_prod)
+	basics.make_ordner(path_dev)
+	basics.make_ordner(path_dev)
+	config.add_section('ENV')
 
-if not(check_port(args["port"])):
-	print "port is not free"
+	root_url 			= raw_input("ROOT_URL: ")
+	mongo_user 			= raw_input("ROOT_URL: ")
+	mongo_pass 			= raw_input("ROOT_URL: ")
+	mongo_ip 			= raw_input("ROOT_URL: ")
+	mongo_port 			= raw_input("ROOT_URL: ")
+	mongo_collection 	= raw_input("ROOT_URL: ")
+
+	config.set('ENV','ROOT_URL',root_url)
+	config.set('ENV','MONGO_URL','mongodb://' + mongo_user + ':' + mongo_pass + '@' + mongo_ip + ':' + mongo_port + '/' + mongo_collection)
+	config.set('ENV','PORT',args["port"])
+	#config.set('ENV','HTTP_FORWARDED_COUNT','1')
+	
+	configfile = open(path_config + "/config.ini",'w')
+	status.write(configfile)
+	configfile.close()
+
+if not check_file(path_config + "/","meteor.conf"):
+	print "config is not present. please run with -i (--init)"
 	sys.exit()
 
-if(args["build"] and ["args.env"] != "develop"):
-	os.chdir(path_dev)
-	basics.command("meteor build --directory " + path_prod)
+config.read(path_config + "/meteor.conf")
 
-if(args["env"] == "develop"):
-	os.chdir(path_dev)
-	basics.command("./set_env.sh")
-	basics.command("meteor --port " + args["port"])
+else:
+	if not(check_port(args["port"])):
+		print "port is not free"
+		sys.exit()
+	
+	if(args["build"] and ["args.env"] != "develop"):
+		os.chdir(path_dev)
+		basics.command("meteor build --directory " + path_prod)
+	
+	if(args["run"]):
+		if(args["env"] == "develop"):
+			os.chdir(path_dev)
+			basics.command("./set_env.sh")
+			basics.command("meteor --port " + args["port"])
+		
+		if(args["env"] == "production"):
+			os.chdir(path_prod + "/bundle/programs/server")
+			basics.command("npm install")
+			os.chdir(path_prod)
+			basics.command("./set_env.sh")
+			basics.command("node bundle/main.js")
 
-if(args["env"] == "production"):
-	os.chdir(path_prod + "/bundle/programs/server")
-	basics.command("npm install")
-	os.chdir(path_prod)
-	basics.command("./set_env.sh")
-	basics.command("node bundle/main.js")
